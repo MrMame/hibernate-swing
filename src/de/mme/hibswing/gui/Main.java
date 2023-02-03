763697180d8f576706d679f8a4fa3c6ebb01fb9a
MrMame <51000524+MrMame@users.noreply.github.com>
2023-02-03 11:29:42 +0100

    changed layout. add button sidebar and menuebar at top of the window

--- src/de/mme/hibswing/gui/Main.java
+++ src/de/mme/hibswing/gui/Main.java
@@ -18,11 +18,20 @@ import javax.swing.JScrollPane;
 import java.awt.event.ActionListener;
 import java.util.Collection;
 import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.BoxLayout;
+import javax.swing.JTable;
 
 public class Main {
 
 	private JFrame frame;
 	private MainWindowController _controller = new MainWindowController();
+	private JTable table;
 
 	
 	/**
@@ -53,48 +62,44 @@ public class Main {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 858);
+		frame.setBounds(100, 100, 638, 858);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+		
+		table = new JTable();
+		panel.add(table);
+		
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, BorderLayout.WEST);
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.Y_AXIS));
+		
+		JButton btnNewButton = new JButton("New button");
+		panel_1.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		panel_1.add(btnNewButton_1);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Connection");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("DB settings...");
+		mnNewMenu.add(mntmNewMenuItem);
 		
 		
 		
 		
 		// Person List
 		DefaultListModel<Person> personsList = new DefaultListModel<>();
-		JList<Person> lstAusgabe = new JList(personsList);		
-				
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 54, 414, 196);
-		scrollPane.setViewportView(lstAusgabe);
-		lstAusgabe.setLayoutOrientation(JList.VERTICAL);
-		frame.getContentPane().add(scrollPane);
 		
 		
-		
-		
-		// Button - GetPersons
-		JButton btnGetAllPersons = new JButton("GetPersons");
-		btnGetAllPersons.setBounds(10, 11, 112, 32);
-		frame.getContentPane().add(btnGetAllPersons);
-		btnGetAllPersons.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				for(Person p : _controller.getAllPersons()){
-					personsList.addElement(p);
-				}		
-			}
-		});
-
-		
-		// Reset DB
-		JButton btnResetDb = new JButton("Rebuild_DB");
-		btnResetDb.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				_controller.rebuildDB();
-			}
-		});
-		btnResetDb.setBounds(132, 10, 95, 33);
-		frame.getContentPane().add(btnResetDb);
+		JList personList = new JList<Person>();
 		
 		
 	}
